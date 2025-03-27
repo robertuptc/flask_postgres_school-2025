6a1eacc763a4d0bb898442b357c8a8ea1f7e4a0f
robertuptc <robertuptc@gmail.com>
2025-03-27 16:33:32 -0400

    repo created

---
+++ school.sql
@@ -0,0 +1,6 @@
+
+-- \COPY students FROM '/home/reginafera13/codeplatoon/flask_postgres_school/data/student.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY teachers FROM '/home/reginafera13/codeplatoon/flask_postgres_school/data/teachers.csv' DELIMITER ',' CSV HEADER;
+
+-- \COPY subjects FROM '/home/reginafera13/codeplatoon/flask_postgres_school/data/subjects.csv' DELIMITER ',' CSV HEADER;
